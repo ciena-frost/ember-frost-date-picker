1125eb17a570f10b615eeb9c552bfeb77c4a8134
Seena Rowhani <seenarowhani95@gmail.com>
2016-10-06 16:56:00 -0400

    Update index.js

--- blueprints/ember-frost-date-picker/index.js
+++ blueprints/ember-frost-date-picker/index.js
@@ -7,9 +7,10 @@ module.exports = {
         {name: 'ember-frost-core', target: '>=0.0.14 <2.0.0'}
       ]
     }).then(function () {
-      return self.addBowerPackagesToProject('moment').then(function () {
-        return self.addBowerPackagesToProject('pikaday')
-      })
+      return self.addBowerPackagesToProject([
+        {name: 'moment'},
+        {name: 'pikaday'}
+      ])
     })
   },
   normalizeEntityName: function () {
