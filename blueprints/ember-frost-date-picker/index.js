faa21966acedd67f92324af82709c241160bc8dc
Seena Rowhani <seenarowhani95@gmail.com>
2016-10-06 15:59:19 -0400

    Update index.js

--- blueprints/ember-frost-date-picker/index.js
+++ blueprints/ember-frost-date-picker/index.js
@@ -11,5 +11,10 @@ module.exports = {
         return self.addBowerPackagesToProject('pikaday')
       })
     })
+  },
+  normalizeEntityName: function () {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
   }
 }
