f76eb431d0f3a8aa49512ff5528747eafbfdb0f2
Matthew Dahl <mdahl@ciena.com>
2017-01-26 08:43:36 -0800

    Fix blueprint

--- blueprints/ember-frost-date-picker/index.js
+++ blueprints/ember-frost-date-picker/index.js
@@ -7,7 +7,7 @@ module.exports = {
         {name: 'ember-frost-core', target: '^1.0.0'},
         {name: 'ember-prop-types', target: '^3.0.0'},
         {name: 'ember-hook', target: '^1.3.5'},
-        {name: 'ember-cli-moment', target: '^1.5.1'},
+        {name: 'ember-cli-moment-shim', target: '^3.0.1'},
         {name: 'ember-pikaday-shim', target: '0.1.0'}
       ]
     }).then(function () {
