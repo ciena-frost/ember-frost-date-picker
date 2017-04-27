54a218a458111de42c634d6982741bbf7268a8d4
Roxanne Panthaky <rpanthak@ciena.com>
2017-04-27 15:03:16 -0400

    add support for engines

--- index.js
+++ index.js
@@ -19,8 +19,25 @@ module.exports = {
     /* eslint-enable no-unused-expressions */
   },
 
-  included: function (app) {
-    this._super.included(app)
-    app.import(`${app.bowerDirectory}/clockpicker-seconds/dist/jquery-clockpicker.min.js`)
+  _findHost: function () {
+    var current = this
+    var app
+
+    // Keep iterating upward until we don't have a grandparent.
+    // Has to do this grandparent check because at some point we hit the project.
+    do {
+      app = current.app || app
+    } while (current.parent.parent && (current = current.parent))
+    return app
+  },
+
+  included: function () {
+    var app = this._findHost()
+
+    if (!app.bowerDirectory) {
+      app.import('bower_components/clockpicker-seconds/dist/jquery-clockpicker.min.js')
+    } else {
+      app.import(`${app.bowerDirectory}/clockpicker-seconds/dist/jquery-clockpicker.min.js`)
+    }
   }
 }
