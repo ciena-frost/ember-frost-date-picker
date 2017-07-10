2af57c5fe994f0a1b043aac4514f9a1c800a1fd9
Qianyi Yu <qyu@ciena.com>
2017-07-10 12:50:30 -0400

    upgrade ember-cli to 2.12.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,3 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module'
-  },
-  extends: 'frost-standard',
-  env: {
-    browser: true
-  },
-  rules: {
-    "ocd/sort-import-declarations": [
-      1,
-      {
-        "localPrefixes": [
-          "../",
-          "./",
-          "dummy/",
-          "ember-frost-date-picker"
-        ]
-      }
-    ]
-  }
+  extends: 'frost-standard'
 }
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,5 +1,4 @@
 /* eslint-env node */
-/* global require, module */
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon')
 
 module.exports = function (defaults) {
