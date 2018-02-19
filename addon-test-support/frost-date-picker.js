9ec16de6ec88b7e8d9647fcec3da143a817e4501
Michael Carroll <juwara0@users.noreply.github.com>
2018-02-19 12:12:45 -0600

    update helper file to be frost-date-picker.js

---
+++ addon-test-support/frost-date-picker.js
@@ -0,0 +1,63 @@
+/* global $ */
+
+import {$hook} from 'ember-hook'
+
+// Helpers taken from ember-pickaday
+
+/**
+ * Open the specified date picker and get an interactor for it
+ * @param {String} hook - the hook for the date picker component
+ * @returns {Object} an object than can selectDate
+ */
+export function openDatepicker (hook) {
+  hook = hook || 'bunsenForm'
+
+  const hookName = `${hook}-input`
+  const $input = $hook(hookName)
+  $input.click()
+
+  return PikadayInteractor
+}
+
+const PikadayInteractor = {
+  selectorForMonthSelect: '.pika-lendar:visible .pika-select-month',
+  selectorForYearSelect: '.pika-lendar:visible .pika-select-year',
+  selectDate: function (date) {
+    const day = date.getDate()
+    const month = date.getMonth()
+    const year = date.getFullYear()
+    const selectEvent = 'ontouchend' in document ? 'touchend' : 'mousedown'
+
+    $(this.selectorForYearSelect).val(year)
+    triggerNativeEvent($(this.selectorForYearSelect)[0], 'change')
+    $(this.selectorForMonthSelect).val(month)
+    triggerNativeEvent($(this.selectorForMonthSelect)[0], 'change')
+
+    triggerNativeEvent($('td[data-day="' + day + '"] button:visible')[0], selectEvent)
+  },
+  selectedDay: function () {
+    return $('.pika-single td.is-selected button').html()
+  },
+  selectedMonth: function () {
+    return $(this.selectorForMonthSelect + ' option:selected').val()
+  },
+  selectedYear: function () {
+    return $(this.selectorForYearSelect + ' option:selected').val()
+  },
+  minimumYear: function () {
+    return $(this.selectorForYearSelect).children().first().val()
+  },
+  maximumYear: function () {
+    return $(this.selectorForYearSelect).children().last().val()
+  }
+}
+
+function triggerNativeEvent (element, eventName) {
+  if (document.createEvent) {
+    let event = document.createEvent('Events')
+    event.initEvent(eventName, true, false)
+    element.dispatchEvent(event)
+  } else {
+    element.fireEvent('on' + eventName)
+  }
+}
